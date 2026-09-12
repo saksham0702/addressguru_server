ede54fa3e78bd3994db88b5251be5f647c24775d
Saksham <sakshammamgain4@gmail.com>
2026-09-12 17:47:21 +0530

    whatsapp template module

--- model/templateSchema.js
+++ model/templateSchema.js
@@ -22,10 +22,36 @@ const templateSchema = new mongoose.Schema(
       required: false,
       trim: true,
     },
+    slug: {
+      type: String,
+      trim: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        "business_pitch",
+        "listing_approved",
+        "listing_rejected",
+        "lead_notification",
+        "claim_verification",
+        "claim_transferred",
+        "claim_rejected",
+        "custom",
+      ],
+      default: "custom",
+    },
+    variables: {
+      type: [String],
+      default: [],
+    },
+    isSystem: {
+      type: Boolean,
+      default: false,
+    },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: false,
     },
     status: {
       type: String,
